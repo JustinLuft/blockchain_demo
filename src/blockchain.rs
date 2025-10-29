8c6c3856abd7f9fc737e98675bceeea14b6b7ada
Justin <89544777+JustinLuft@users.noreply.github.com>
2025-10-29 16:03:35 -0400

    Update blockchain.rs

--- src/blockchain.rs
+++ src/blockchain.rs
@@ -1,1 +1,31 @@
 
+use crate::blocks::block::Block;
+
+pub struct Blockchain {
+    pub chain: Vec<Block>,
+    pub difficulty: usize,
+}
+
+impl Blockchain {
+    pub fn new(difficulty: usize) -> Self {
+        let genesis_block = Block::new(0, "Genesis Block".to_string(), "0".to_string(), difficulty);
+        Blockchain { chain: vec![genesis_block], difficulty }
+    }
+
+    pub fn add_block(&mut self, data: String) {
+        let last_block = self.chain.last().unwrap();
+        let new_block = Block::new(
+            last_block.index + 1,
+            data,
+            last_block.hash.clone(),
+            self.difficulty,
+        );
+        self.chain.push(new_block);
+    }
+
+    pub fn print_chain(&self) {
+        for block in &self.chain {
+            println!("{:?}", block);
+        }
+    }
+}
